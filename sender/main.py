295985e4997c877c310318fc9863d807bb4b2d8b
andygcui <andygcui@gmail.com>
2026-02-24 09:20:21 -0500

    initial publish

---
+++ sender/main.py
@@ -0,0 +1,14 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from pythonosc.udp_client import SimpleUDPClient
+
+# osc settings
+OSC_IP = "127.0.0.1"
+OSC_PORT = 8000
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+if __name__ == "__main__":
+    main()
