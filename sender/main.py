e1197a4ab15fef04cbe99a066acac4a5f6973597
andygcui <andygcui@gmail.com>
2026-02-24 10:13:21 -0500

    fix bugs

--- sender/main.py
+++ sender/main.py
@@ -46,28 +46,8 @@ with mp_hands.Hands(
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
 
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-
-        SHOW_NEAR_POINTS = True
-        if SHOW_NEAR_POINTS:
-            h, w = image.shape[:2]
-            for j in range(21):
-                px, py = int(lm[j].x * w), int(lm[j].y * h)
-                cv2.putText(image, f"{j}", (px + 4, py - 4),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
-                        
-        print(hand_landmarks)
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
+    
+    # flip the image horizontally
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
       break
